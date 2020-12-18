c95eff37863c79d7568bee482616020b9e8d1216
su530201 <olivier.kaufmann@umons.ac.be>
2020-12-18 17:04:39 +0100

    trying to use orm with omf

--- core/core.py
+++ core/core.py
@@ -1,8 +1,10 @@
-from core.orm import BoreholeOrm
+from core.orm import BoreholeOrm, ComponentOrm
+
 
 class Project:
     def __init__(self, session):
         self.session = session
+        self.boreholes = None
         self.refresh()
 
     def refresh(self):
@@ -13,5 +15,12 @@ class Project:
         
     def add_borehole(self, bh):
         self.session.add(bh)
+        self.commit()
+        self.refresh()
+
+    def add_components(self, components):
+        for comp_id in components.keys():
+            new_component = ComponentOrm(id=comp_id, description=components[comp_id].summary())
+            self.session.add(new_component)
         self.commit()
         self.refresh()
