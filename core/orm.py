8b6fdc3d6dce850137051006b03261d5e2531614
CampeolQ <quentin.campeol@student.umons.ac.be>
2020-12-22 11:51:03 +0100

    Writing the docstings for the core.orm module

--- core/orm.py
+++ core/orm.py
@@ -8,7 +8,18 @@ Base = declarative_base()
 
 
 class BoreholeOrm(Base):
-    """The Boreholes Info table"""
+    """The Boreholes Info table
+    
+    Attributes
+    ----------
+    id : str
+        The id of the borehole.
+        
+    See Also
+    --------
+    IntervalOrm : Relationship one to many with the IntervalOrm table.
+
+    """
     __tablename__ = 'Boreholes'
     id = Column(String(32), primary_key=True)
     intervals = relationship(
@@ -21,7 +32,26 @@ class BoreholeOrm(Base):
                                          top=v['top'], base=v['base']))
 
 class PositionOrm(Base):
-    """The Position table"""
+    """The Position table
+    
+    Attributes
+    ----------
+    id : int
+        The id of the interval, different for each borehole interval.
+    upper : float
+           The upper value of the position.
+    middle : float
+            The middle value of the position.
+    lower : float
+           The lower value of the position.
+    x : float
+        The X coordinate.
+    y : float
+        The Y coordinate.
+    z : float
+        The Y coordinate, synonym of the middle attribute.    
+
+    """
     __tablename__ = 'Positions'
     id = Column(Integer, primary_key=True)
     upper = Column(Float(32))
@@ -33,7 +63,29 @@ class PositionOrm(Base):
 
 
 class IntervalOrm(Base):
-    """The Interval table"""
+    """The Interval table
+        
+    Attributes
+    ----------
+    id : int
+        The id of the interval, different for each borehole interval.
+    borehole : str
+              The name of the borehole from which the interval originated.
+    interval_number : integer 
+                     The number the interval in the borehole, starts at 0 for the upper interval of each different borehole.
+    description : str
+                 The name of the main component in the interval.
+    top_id : int
+            The id of the top position of the interval, link to the position id in the PositionOrm table.
+    base_id : int
+             The id of the base position of the interval, link to the position id in the PositionOrm table.
+             
+    See Also
+    --------
+    ComponentOrm : Relationship many to many with the Intervals table using the intermediate LinkIntervalComponentOrm table.
+    PositionOrm : Relationship many to one with the Intervals table
+    
+    """
     __tablename__ = 'Intervals'
     id = Column(Integer, primary_key=True)
     borehole = Column(String(32), ForeignKey('Boreholes.id'))
@@ -47,7 +99,20 @@ class IntervalOrm(Base):
 
 
 class ComponentOrm(Base):
-    """The Component table"""
+    """The Component table
+    
+    Attributes
+    ----------
+    id : str
+        The id of the component.
+    description : str
+                 The name of the component.
+        
+    See Also
+    --------
+    IntervalOrm : Relationship one to many with the IntervalOrm table.
+
+    """
     __tablename__ = 'Components'
     id = Column(String(32), primary_key=True)
     intervals = relationship(IntervalOrm, secondary='Linkintervalcomponent')
@@ -55,7 +120,16 @@ class ComponentOrm(Base):
 
 
 class LinkIntervalComponentOrm(Base):
-    """The junction table between component and interval"""
+    """The junction table between component and interval
+    
+    Attributes
+    ----------
+    id : int
+        The id of the interval, different for each borehole interval.
+    description : str
+                 The name of the component.
+    
+    """
     __tablename__ = 'Linkintervalcomponent'
 
     int_id = Column(
